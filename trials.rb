a7da989e3fbcded2d9f45b159e2b4e84e7362ea6
Chris McCammon <cmccammon@gmail.com>
2016-01-04 11:44:25 -0600

    added more basics

--- trials.rb
+++ trials.rb
@@ -1,8 +1,36 @@
-x =1
-loop do
-  x += 1
-  next unless (x %2) == 0
-  puts x
+# x =1
+# loop do
+#   x += 1
+#   next unless (x %2) == 0
+#   puts x
+#
+#   break if x >= 10
+# end
 
-  break if x >= 10
-end
+# y =1
+#
+# while y <= 45
+#   y += 1
+#   next unless (y % 2) == 0
+#   puts y
+# end
+
+# numbers = [1,2,3,4,5,6,7,8]
+#
+# for number in numbers
+#   puts "#{number}"
+# end
+
+# (0..20).each do |i|
+#   if i % 3 == 0
+#     puts "cheese"
+#   else
+#     puts i
+#   end
+# end
+#
+# def add_nums(num_1, num_2)
+#   return num_1.to_i + num_2.to_i  # => 10
+# end
+#
+# add_nums(4, 6)  # => 10
