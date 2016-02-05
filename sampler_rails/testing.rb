d5b471e63ee613188c357e7394d156b34942feea
Chris McCammon <cmccammon@gmail.com>
2016-02-05 13:43:55 -0600

    odd even

--- sampler_rails/testing.rb
+++ sampler_rails/testing.rb
@@ -9,7 +9,19 @@
 # staircase(6)
 n = 6
 x = 0
-while x <= n - 1
-    x += 1
-    puts " " * ( n - x ) + "#" * x
+# while x <= n - 1
+#     x += 1
+#     puts " " * ( n - x ) + "#" * x
+# end
+
+def check(n)
+  (1..n).each do |x|
+    if x.even?
+      puts "#{x} is even"
+    elsif x.odd?
+      puts "#{x} is odd"
+    end
+  end
 end
+
+check(15)
